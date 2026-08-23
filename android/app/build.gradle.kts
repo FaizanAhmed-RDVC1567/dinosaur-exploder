8f748120920c3aa09e2104c0a5a1a88886582283
Faizan Ahmed <chfaizan.510@outlook.com>
2026-08-23 13:10:50 +0400

    fix: Successfully added the FXGL dependency line to the module-level build.gradle.kts file.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.gluonfx) // Enabling the plugin here too. CHECK IF IT WORKS
 }
 
 android {
@@ -39,6 +40,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+    // Adding the line for FXGL
+    implementation(libs.fxgl)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
