e680b6238dbd86150a448df437e7ec4496806bba
Faizan Ahmed <chfaizan.510@outlook.com>
2026-08-23 13:01:15 +0400

    fix: added line to enable GluonFX plugin throughout project.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    // Attempting to apply gluonfx plugin here
+    alias(libs.plugins.gluonfx) apply false
 }
