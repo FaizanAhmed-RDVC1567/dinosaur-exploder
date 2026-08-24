e507ce2a6538c92276c9ed523e03e13402cb6b80
Faizan Ahmed <chfaizan.510@outlook.com>
2026-08-24 12:51:53 +0400

    Add extra helpful comment.

--- android/app/src/main/java/com/jvondermarck/dinosaurexploder/Bootstrap.java
+++ android/app/src/main/java/com/jvondermarck/dinosaurexploder/Bootstrap.java
@@ -5,6 +5,7 @@ import com.almasb.fxgl.app.GameApplication;
 public class Bootstrap {
     public static void main(String[] args) {
         // Launch the FXGL GameApplication subclass (MainActivity.kt)
+        // GluonFX actually expects a `main`-style entry to call, which this class provides.
         GameApplication.launch(MainActivity.class, args);
     }
 }
