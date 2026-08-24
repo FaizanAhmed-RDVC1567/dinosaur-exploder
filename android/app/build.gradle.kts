f6856c6798c26bdc56e3a6b6899f43a238c69567
Faizan Ahmed <chfaizan.510@outlook.com>
2026-08-24 12:46:43 +0400

    fix: introduced fixes to prevent Gradle sync issues by using a Bootstrap class and extraProperties for Kotlin DSL.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,23 @@ plugins {
     alias(libs.plugins.gluonfx) // Enabling the plugin here too. CHECK IF IT WORKS
 }
 
+/*Some code to prevent Kotlin DSL strict type checking when Gradle attempts to confirm the value
+* of GluonFX's dynamic DSL property called `mainClass`. Gradle simply cannot process this value
+* at compile time, so this code is an attempt at a workaround to prevent Gradle frequently
+* choking itself on this plugin's DSL value.
+*
+* This current approach usestwo things:
+* 1) Creating an unambiguous JVM entry point (Bootstrap) with a standard main method that GluonFX
+* will use.
+*
+* 2) the `extraProperties` accessor, so that Kotlin DSL won't choke on the
+* value, but will still expose this property to the GluonFX plugin at runtime, as it will avoid
+* Kotlin DSL's compile-time checks when Gradle is executed.
+*
+* Something else that is recommended, is to fall back to Gradle version 8.6 instead of 9.0+,
+* as those versions reportedly have better compatibility with GluonFX version 1.0.29*/
+project.extensions.extraProperties["gluonfx.mainClass"] = "com.jvondermarck.dinosaurexploder.Bootstrap"
+
 android {
     namespace = "com.jvondermarck.dinosaurexploder"
     compileSdk {
@@ -37,7 +54,9 @@ android {
 // GluonFX settings for the application
 gluonfx {
     target = "android" // If working on iOS, this value will be set to that
-    // `mainClass` or `mainClassName` is not required in newer GluonFX versions
+    // `mainClass` is required, the GluonFX plugin's DSL is dynamic and Gradle will pick it up at
+    // runtime
+    // mainClass = "com.jvondermarck.dinosaurexploder.MainActivity"
     attachConfig {
         version = "4.0.25"  // This identifier & value is required in newer versions
         configuration = "implementation"
