19b851b032e7aa9165da4cf41b894ba835163638
Faizan Ahmed <chfaizan.510@outlook.com>
2026-09-09 23:13:34 +0500

    chore: Add explanatory comment to the class
    The comments explains the use of declaring a Kotlin class on a single line with a default value, which cuts down boilerplate code from Java.

--- android/app/src/main/java/com/jvondermarck/dinosaurexploder/model/TotalCoins.kt
+++ android/app/src/main/java/com/jvondermarck/dinosaurexploder/model/TotalCoins.kt
@@ -3,7 +3,18 @@ package com.jvondermarck.dinosaurexploder.model
 import java.io.Serializable
 
 class TotalCoins(private var total: Int = 0) : Serializable {
-
+    /*
+    * The syntax above handles both type of Java constructor calls:
+    *
+    * a) where `total` is not specified, e.g.: TotalCoins(),
+    *    in which case the default value of 0 is used.
+    *
+    * b) where a value for `total` is specified, e.g.: TotalCoins(125),
+    *    in which case the specified value is used instead.
+    *
+    * In summary, it simplifies lots of Java boilerplate code down to a single line,
+    * while still keeping the code readable.
+    * */
     fun getTotal(): Int {
         return total
     }
