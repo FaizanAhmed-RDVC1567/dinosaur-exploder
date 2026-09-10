03da3e6dcb95a4c1aa3b192b8bca3631af1a8229
Faizan Ahmed <chfaizan.510@outlook.com>
2026-09-10 21:50:04 +0500

    feat: add stub for managing sound settings
    The class will allow to set SFX and volume values, along with boolean values for whether the volume or SFX is muted. There is an additional property called 'language' which shall be used later on when more features of the game will be added.

---
+++ android/app/src/main/java/com/jvondermarck/dinosaurexploder/model/Settings.kt
@@ -0,0 +1,10 @@
+package com.jvondermarck.dinosaurexploder.model
+
+class Settings {
+
+    var volume: Double? = null
+    var sfxVolume: Double? = null
+    var muted: Boolean? = null
+    var sfxMuted: Boolean? = null
+    lateinit var language: String
+}
